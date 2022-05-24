58a15a58d8a516d523389ed78594cf960264243b
sanketpatel32 <sanpatel323@gmail.com>
2022-05-24 15:18:11 +0530

    Emotion detection added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, Response , request , url_for,redirect,flash
 from Frame import get_frame
+import numpy as np 
+import cv2
+from tensorflow.keras.models import load_model
 import psycopg2
 import psycopg2.extras
 from StudentData import auth_users
@@ -103,6 +106,44 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/collecting')
+def collecting():
+	return render_template('collecting.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+	image = request.files['select_file']
+	image.save('./static/file.jpg')
+	image = cv2.imread('static/file.jpg')
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+	faces = cascade.detectMultiScale(gray, 1.1, 3)
+	for x,y,w,h in faces:
+		cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+		cropped = image[y:y+h, x:x+w]
+	cv2.imwrite('static/after.jpg', image)
+	try:
+		cv2.imwrite('static/cropped.jpg', cropped)
+	except:
+		pass
+	try:
+		img = cv2.imread('static/cropped.jpg', 0)
+	except:
+		img = cv2.imread('static/file.jpg', 0)
+	img = cv2.resize(img, (48,48))
+	img = img/255
+	img = img.reshape(1,48,48,1)
+	model = load_model('model.h5')
+	pred = model.predict(img)
+	label_map = ['Anger','Neutral' , 'Fear', 'Happy', 'Sad', 'Surprise']
+	pred = np.argmax(pred)
+	final_pred = label_map[pred]
+	return render_template('predict.html', data=final_pred)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True ,port=8000,use_reloader=False)
